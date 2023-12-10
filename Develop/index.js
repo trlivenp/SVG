7e6ad1fd51a3e7569560f710dbb5cad0c5ace833
Tristan Livengood <tlivengood55@gmail.com>
2023-12-10 00:45:38 -0500

    Fixed the text not dispalying

--- Develop/index.js
+++ Develop/index.js
@@ -2,18 +2,34 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 
 function createSVG(userInput) {
-  const { text, textColor, shape, shapeColor } = userInput;
+  const { text, textColor = '#000000', shape, shapeColor } = userInput;
 
   // Basic validation - ensure required fields are present
-  if (!text || !textColor || !shape || !shapeColor) {
+  if (!text || !shape || !shapeColor) {
     throw new Error('Invalid user input. Please provide all required values.');
+  }
+
+  let shapeElement = '';
+
+  switch (shape) {
+    case 'circle':
+      shapeElement = `<circle cx="150" cy="100" r="50" fill="${shapeColor}" />`;
+      break;
+    case 'triangle':
+      shapeElement = `<polygon points="150,50 100,150 200,150" fill="${shapeColor}" />`;
+      break;
+    case 'square':
+      shapeElement = `<rect x="100" y="50" width="100" height="100" fill="${shapeColor}" />`;
+      break;
+    default:
+      throw new Error(`Unsupported shape: ${shape}`);
   }
 
   // SVG template based on the user input
   const svgContent = `
     <svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-      <text x="50%" y="50%" font-size="20" fill="${textColor}" text-anchor="middle">${text}</text>
-      <${shape} cx="150" cy="100" r="50" fill="${shapeColor}" />
+      ${shapeElement}
+      <text x="50%" y="50%" font-size="20" fill="${textColor}" text-anchor="middle" dominant-baseline="middle">${text}</text>
     </svg>
   `;
 
