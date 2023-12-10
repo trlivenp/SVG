8530e16b59850886104062e2ed246db6c7520549
Tristan Livengood <tlivengood55@gmail.com>
2023-12-10 00:26:43 -0500

    finally figured out why inquirer was not working, has an example svglogo file

--- Develop/index.js
+++ Develop/index.js
@@ -1,5 +1,30 @@
-const inquirer = require('inquirer'); // Third-party library for command-line user input
-const generateSVG = require('./generateSVG'); // Separate module that generates SVG content
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+function createSVG(userInput) {
+  const { text, textColor, shape, shapeColor } = userInput;
+
+  // Basic validation - ensure required fields are present
+  if (!text || !textColor || !shape || !shapeColor) {
+    throw new Error('Invalid user input. Please provide all required values.');
+  }
+
+  // SVG template based on the user input
+  const svgContent = `
+    <svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+      <text x="50%" y="50%" font-size="20" fill="${textColor}" text-anchor="middle">${text}</text>
+      <${shape} cx="150" cy="100" r="50" fill="${shapeColor}" />
+    </svg>
+  `;
+
+  return svgContent;
+}
+
+function generateSVG(userInput) {
+  const svgContent = createSVG(userInput);
+
+  fs.writeFileSync('logo.svg', svgContent);
+}
 
 async function commandLineApp() {
   const userInput = await inquirer.prompt([
@@ -32,4 +57,6 @@ async function commandLineApp() {
   console.log('Generated logo.svg');
 }
 
-module.exports = commandLineApp;
+// Run the command line app
+commandLineApp();
+
---
+++ Develop/index.test.js
@@ -0,0 +1,54 @@
+const commandLineApp = require('./index.js').default;
+
+describe('commandLineApp', () => {
+  // Test case 1: Valid input
+  test('should generate SVG for valid input', async () => {
+    // Using Jest's mock functions to mock dependencies
+    // Mock the user input
+    const mockInput = {
+      text: 'ABC',
+      textColor: 'red',
+      shape: 'circle',
+      shapeColor: '#00FF00',
+    };
+
+    // Mock inquirer.prompt to return the user input
+    jest.spyOn(inquirer, 'prompt').mockResolvedValue(mockInput);
+
+    // Mock fs.writeFileSync to avoid actual file writing during tests
+    jest.spyOn(fs, 'writeFileSync').mockImplementation(() => {});
+
+    // Run the function
+    await commandLineApp();
+
+    // Assert that inquirer.prompt was called with the correct questions
+    expect(inquirer.prompt).toHaveBeenCalledWith(/* questions array */);
+
+    // Assert that fs.writeFileSync was called with the correct filename and content
+    expect(fs.writeFileSync).toHaveBeenCalledWith('logo.svg', /* expected SVG content */);
+
+    // Assert console.log output
+    expect(console.log).toHaveBeenCalledWith('Generated logo.svg');
+  });
+
+  // Test case 2: Input validation
+  test('should handle invalid input with proper validation message', async () => {
+    // Mock inquirer.prompt to simulate invalid input (e.g., text longer than 3 characters)
+    jest.spyOn(inquirer, 'prompt').mockResolvedValue({ text: 'InvalidInput', textColor: 'red', shape: 'circle', shapeColor: '#00FF00' });
+
+    // Mock fs.writeFileSync to avoid actual file writing during tests
+    jest.spyOn(fs, 'writeFileSync').mockImplementation(() => {});
+
+    // Run the function
+    await commandLineApp();
+
+    // Assert that inquirer.prompt was called with the correct questions
+    expect(inquirer.prompt).toHaveBeenCalledWith(/* questions array */);
+
+    
+    expect(console.log).toHaveBeenCalledWith('Generated logo.svg');
+    
+  });
+
+  
+});
